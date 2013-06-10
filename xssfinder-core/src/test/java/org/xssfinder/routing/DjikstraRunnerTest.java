35ccb4a996323f5fce74b963b67a55b708964606
Rowan Hill <rowan.hill@softwire.com>
2013-06-10 22:41:20 +0100

    Simple test for DjikstraRunner

---
+++ xssfinder-core/src/test/java/org/xssfinder/routing/DjikstraRunnerTest.java
@@ -0,0 +1,76 @@
+package org.xssfinder.routing;
+
+import com.google.common.collect.ImmutableSet;
+import org.junit.Test;
+import org.xssfinder.Page;
+
+import java.util.Set;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DjikstraRunnerTest {
+    @Test
+    public void findsShortestPathsToLeafNodes() {
+        // given
+        Set<PageDescriptor> descriptors = ImmutableSet.of(
+                new PageDescriptor(RootPage.class),
+                new PageDescriptor(MiddlePage.class),
+                new PageDescriptor(DetourPageOne.class),
+                new PageDescriptor(DetourPageTwo.class),
+                new PageDescriptor(LeafPageOne.class),
+                new PageDescriptor(LeafPageTwo.class)
+        );
+        GraphNodesFactory nodesFactory = new GraphNodesFactory();
+        DjikstraRunner runner = new DjikstraRunner(descriptors, nodesFactory);
+
+        // when
+        Set<GraphNode> leafNodes = runner.findShortestPathsAndReturnLeafNodes(RootPage.class);
+
+        // then
+        assertThat(leafNodes.size(), is(4));
+        for (GraphNode node : leafNodes) {
+            assertThat(node.getDistance(), is(2));
+            assertThat(node.getPredecessor().getPageClass() == MiddlePage.class, is(true));
+        }
+    }
+
+    /**
+     * The page network is arranged as below:
+     *
+     *         LeafOne         LeafTwo
+     *        /      \        /       \
+     *  DetourOne     \      /       DetourTwo
+     *    |            \    /              |
+     *    |-------------Middle-------------|
+     *                    |
+     *                   Root
+     */
+    @SuppressWarnings("UnusedDeclaration")
+    @Page
+    private static class RootPage {
+        public MiddlePage getMiddlePage() { return null; }
+    }
+    @SuppressWarnings("UnusedDeclaration")
+    @Page
+    private static class MiddlePage {
+        public DetourPageOne getDetourPageOne() { return null; }
+        public DetourPageTwo getDetourPageTwo() { return null; }
+        public LeafPageOne getLeafPageOne() { return null; }
+        public LeafPageTwo getLeafPageTwo() { return null; }
+    }
+    @SuppressWarnings("UnusedDeclaration")
+    @Page
+    private static class DetourPageOne {
+        public LeafPageOne getLeafPageOne() { return null; }
+    }
+    @Page
+    private static class LeafPageOne {}
+    @SuppressWarnings("UnusedDeclaration")
+    @Page
+    private static class DetourPageTwo {
+        public LeafPageTwo getLeafPageTwo() { return null; }
+    }
+    @Page
+    private static class LeafPageTwo {}
+}
