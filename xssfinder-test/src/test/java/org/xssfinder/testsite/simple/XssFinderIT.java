c288039f9e592136a679fd6f567a1c058f9dc010
Rowan Hill <rowan.hill@softwire.com>
2013-07-21 18:43:44 +0100

    WIP Thrift refactoring:  - Refactor into XssFinder

--- xssfinder-test/src/test/java/org/xssfinder/testsite/simple/XssFinderIT.java
+++ xssfinder-test/src/test/java/org/xssfinder/testsite/simple/XssFinderIT.java
@@ -1,30 +1,17 @@
 package org.xssfinder.testsite.simple;
 
-import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.transport.TSocket;
-import org.apache.thrift.transport.TTransport;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.xssfinder.remote.Executor;
+import org.xssfinder.XssFinder;
 import org.xssfinder.remote.ExecutorServer;
-import org.xssfinder.remote.ExecutorWrapper;
-import org.xssfinder.remote.PageDefinition;
 import org.xssfinder.reporting.XssJournal;
 import org.xssfinder.reporting.XssSighting;
-import org.xssfinder.routing.Route;
-import org.xssfinder.routing.RouteGenerator;
-import org.xssfinder.routing.RouteGeneratorFactory;
-import org.xssfinder.runner.RouteRunner;
-import org.xssfinder.runner.RouteRunnerFactory;
 import org.xssfinder.scanner.ExecutorServerFactory;
 
 import java.io.File;
-import java.util.List;
-import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
@@ -59,10 +46,9 @@ public class XssFinderIT {
 
     @Test
     public void runXssFinder() throws Exception {
-        // Set up the remote executor
+        // given
         ExecutorServerFactory executorServerFactory = new ExecutorServerFactory();
         final ExecutorServer executorServer = executorServerFactory.createExecutorServer(9091);
-
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -72,38 +58,18 @@ public class XssFinderIT {
         thread.setDaemon(true);
         thread.start();
 
-        // TODO: Encapsulate creating the coordinator & getting the routes into a new class. Should be a one-liner.
-        // Set up the coordinator
-        RouteRunnerFactory runnerFactory = new RouteRunnerFactory();
-        TTransport transport = new TSocket("localhost", 9091, 5000);
-        Executor.Client client;
-        try {
-            transport.open();
-            TProtocol protocol = new TBinaryProtocol(transport);
-            client = new Executor.Client(protocol);
-            ExecutorWrapper executorWrapper = new ExecutorWrapper(client);
-            RouteRunner runner = runnerFactory.createRouteRunner(executorWrapper, OUTPUT_FILE);
+        // when
+        XssFinder xssFinder = new XssFinder("localhost", 9091, 5000);
+        XssJournal journal = xssFinder.findXssVulnerabilities("org.xssfinder.testsite.simple", OUTPUT_FILE);
 
-            // Get the routes
-            Set<PageDefinition> pageDefinitions = client.getPageDefinitions("org.xssfinder.testsite.simple");
-            RouteGeneratorFactory routeGeneratorFactory = new RouteGeneratorFactory();
-            RouteGenerator routeGenerator = routeGeneratorFactory.createRouteGenerator();
-            List<Route> routes = routeGenerator.generateRoutes(pageDefinitions);
-
-            // Run!
-            XssJournal journal = runner.run(routes);
-
-            assertThat(routes.size(), is(4));
-            assertThat(journal.getDescriptorById("1"), is(not(nullValue())));
-            assertThat(journal.getXssSightings().size(), is(1));
-            assertThat(journal.getErrorContexts().size(), is(2)); // throwException is called on both runs (attack / observe)
-            XssSighting sighting = journal.getXssSightings().iterator().next();
-            assertThat(sighting.getSubmitMethodName(), is("unsafeSubmit"));
-            assertThat(sighting.getInputIdentifier(), is("//form[@id=\"unsafeForm\"]/input[1]"));
-            assertThat(journal.getPagesClassWithUntestedInputs().size(), is(1));
-            assertThat(new File(OUTPUT_FILE).exists(), is(true));
-        } finally {
-            transport.close();
-        }
+        // then
+        assertThat(journal.getDescriptorById("1"), is(not(nullValue())));
+        assertThat(journal.getXssSightings().size(), is(1));
+        assertThat(journal.getErrorContexts().size(), is(2)); // throwException is called on both runs (attack / observe)
+        XssSighting sighting = journal.getXssSightings().iterator().next();
+        assertThat(sighting.getSubmitMethodName(), is("unsafeSubmit"));
+        assertThat(sighting.getInputIdentifier(), is("//form[@id=\"unsafeForm\"]/input[1]"));
+        assertThat(journal.getPagesClassWithUntestedInputs().size(), is(1));
+        assertThat(new File(OUTPUT_FILE).exists(), is(true));
     }
 }
