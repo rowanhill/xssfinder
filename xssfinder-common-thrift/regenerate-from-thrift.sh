92047b60dcd4010b6370f1485249c31b2423c960
Rowan Hill <rowan.hill@softwire.com>
2013-08-04 17:21:14 +0100

    Add PHP Thrift client generation

--- xssfinder-common-thrift/regenerate-from-thrift.sh
+++ xssfinder-common-thrift/regenerate-from-thrift.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 THRIFT_BIN=/usr/local/bin
 JAVA_SRC=../xssfinder-java-thrift/src/main/java
+PHP_SRC=../xssfinder-php-executor/src
 
 rm -rf $JAVA_SRC
 mkdir -p $JAVA_SRC
 $THRIFT_BIN/thrift -out $JAVA_SRC --gen java remote-runner.thrift
+
+rm -rf $PHP_SRC
+mkdir -p $PHP_SRC
+$THRIFT_BIN/thrift -out $PHP_SRC --gen php:namespace remote-runner.thrift
--- xssfinder-common-thrift/remote-runner.thrift
+++ xssfinder-common-thrift/remote-runner.thrift
@@ -1,4 +1,5 @@
 namespace java org.xssfinder.remote
+namespace php XssFinderRemote
 
 struct PageDefinition {}
 
