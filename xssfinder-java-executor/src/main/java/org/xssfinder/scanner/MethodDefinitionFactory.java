72637497a4d095bbe5d06ee3b6e1a04f604f3bdd
Rowan Hill <rowan.hill@softwire.com>
2013-08-28 22:53:29 +0100

    Simplify MethodDefinitionFactory by removing PageDefinitionFactory dependency

--- xssfinder-java-executor/src/main/java/org/xssfinder/scanner/MethodDefinitionFactory.java
+++ xssfinder-java-executor/src/main/java/org/xssfinder/scanner/MethodDefinitionFactory.java
@@ -8,31 +8,20 @@ import org.xssfinder.remote.PageDefinition;
 import java.lang.reflect.Method;
 
 public class MethodDefinitionFactory {
-    public MethodDefinition createMethodDefinition(
-            Method method,
-            PageDefinitionFactory pageDefinitionFactory
-    ) {
+    public MethodDefinition createMethodDefinition(Method method) {
         String identifier = method.getName();
         Class<?> returnTypeClass = method.getReturnType();
-        PageDefinition returnType = getPageDefinition(pageDefinitionFactory, returnTypeClass);
         Class<?> owningTypeClass = method.getDeclaringClass();
         boolean hasArgs = method.getParameterTypes().length > 0;
         boolean isSubmit = method.isAnnotationPresent(SubmitAction.class);
         boolean hasCustomTraverser = method.isAnnotationPresent(TraverseWith.class);
         return new MethodDefinition(
                 identifier,
-                returnType.getIdentifier(),
+                returnTypeClass.getCanonicalName(),
                 owningTypeClass.getCanonicalName(),
                 hasArgs,
                 isSubmit,
                 hasCustomTraverser
         );
-    }
-
-    private PageDefinition getPageDefinition(
-            PageDefinitionFactory pageDefinitionFactory,
-            Class<?> returnTypeClass
-    ) {
-        return pageDefinitionFactory.createPageDefinition(returnTypeClass);
     }
 }
