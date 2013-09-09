71c25bbcefd945e8724e26f607df98bbd1b841f5
Rowan Hill <rowan.hill@softwire.com>
2013-09-09 18:36:55 +0100

    Add phpdoc comments & stub return values to ExecutorHandler

--- xssfinder-php-executor/src/XssFinder/Remote/ExecutorHandler.php
+++ xssfinder-php-executor/src/XssFinder/Remote/ExecutorHandler.php
@@ -35,6 +35,12 @@ class ExecutorHandler implements ExecutorIf
         $this->_classNames = $classNames;
     }
 
+    /**
+     * Gets definitions of all page objects in the given namespace
+     *
+     * @param string $namespaceIdentifier The namespace to filter pages by
+     * @return array An array of PageDefinition objects
+     */
     public function getPageDefinitions($namespaceIdentifier)
     {
         $pageFinder = $this->_pageFinderFactory->createPageFinder($namespaceIdentifier);
@@ -48,31 +54,63 @@ class ExecutorHandler implements ExecutorIf
         return $pageDefinitions;
     }
 
+    /**
+     * Navigate the driver to the URL associated with the specified PageDefinition
+     *
+     * @param string $pageIdentifier The identifier of the crawlStartPoint PageDefinition to start the root at
+     */
     public function startRoute($pageIdentifier)
     {
         $this->_executorContext->visitUrlOfRootPage($pageIdentifier);
     }
 
+    /**
+     * Put XSS attacks into all available inputs
+     *
+     * @return array A map of input identifiers -> attack identifiers
+     */
     public function putXssAttackStringsInInputs()
     {
         // TODO: Implement putXssAttackStringsInInputs() method.
+        return array();
     }
 
+    /**
+     * @return array The set of currently XSS attack identifiers observable on the current page
+     */
     public function getCurrentXssIds()
     {
         // TODO: Implement getCurrentXssIds() method.
+        return array();
     }
 
+    /**
+     * @return int The number of forms observable on the current page
+     */
     public function getFormCount()
     {
         // TODO: Implement getFormCount() method.
+        return 0;
     }
 
+    /**
+     * Traverse the given method on the current page object in the given mode
+     *
+     * @param MethodDefinition $method The method to invoke on the current page object
+     * @param string $mode The mode in which to traverse the method
+     * @return array A map of input identifiers -> attack identifiers
+     */
     public function traverseMethod(MethodDefinition $method, $mode)
     {
         // TODO: Implement traverseMethod() method.
+        return array();
     }
 
+    /**
+     * Invoke the 'after route' event handler for a route starting at the identified page
+     *
+     * @param string $rootPageIdentifier The root page of the route that has just finished
+     */
     public function invokeAfterRouteHandler($rootPageIdentifier)
     {
         // TODO: Implement invokeAfterRouteHandler() method.
