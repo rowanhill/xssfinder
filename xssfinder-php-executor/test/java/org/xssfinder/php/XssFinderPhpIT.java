fdc56bb2f455d154604a1874c010f6a1cd56f024
Rowan Hill <rowan.hill@softwire.com>
2013-11-30 21:51:13 +0000

    Run Selenium server in PHP integration test

--- xssfinder-php-executor/test/java/org/xssfinder/php/XssFinderPhpIT.java
+++ xssfinder-php-executor/test/java/org/xssfinder/php/XssFinderPhpIT.java
@@ -2,14 +2,14 @@ package org.xssfinder.php;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.xssfinder.XssFinder;
 import org.xssfinder.reporting.XssJournal;
 import org.xssfinder.reporting.XssSighting;
 
 import java.io.*;
+import java.net.*;
+import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
@@ -21,30 +21,21 @@ public class XssFinderPhpIT
 {
     private static final String OUTPUT_FILE = "xssfinder_int_test_report.html";
 
-    private Process serverProcess;
+    private static Process seleniumProcess = null;
 
-    @Before
-    public void setUp() throws Exception {
-        if (System.getProperty("jetty.port") != null) {
-            // If we're running in Maven, Jetty is already running
-            return;
+    private Process serverProcess = null;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        startSelenium();
+        startTestWebApp();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        if (seleniumProcess != null) {
+            seleniumProcess.destroy();
         }
-
-        // Start the Jetty server running, referencing the xssfinder-testwebapp project. Note: this is slightly
-        // different to the maven tests, which builds the war first and deploys that. This shouldn't have any
-        // impact on the tests.
-
-        Server server = new Server(8085);
-        server.setStopAtShutdown(true);
-
-        WebAppContext webAppContext = new WebAppContext();
-        webAppContext.setContextPath("/");
-        webAppContext.setResourceBase("xssfinder-testwebapp/src/main/webapp");
-        webAppContext.setDescriptor("WEB-INF/web.xml");
-        webAppContext.setClassLoader(getClass().getClassLoader());
-        server.setHandler(webAppContext);
-
-        server.start();
     }
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
@@ -82,30 +73,87 @@ public class XssFinderPhpIT
         assertThat(new File(OUTPUT_FILE).exists(), is(true));
     }
 
-    private void startPhpExecutor() {
+    private static void startSelenium() throws Exception {
+        try {
+            ProcessBuilder pb = new ProcessBuilder();
+            pb.command("java", "-jar", "vendor/selenium/selenium-server-standalone-2.35.0.jar");
+            seleniumProcess = pb.start();
+
+            // Read from standard out & error
+            StreamGobbler errorGobbler = new StreamGobbler(seleniumProcess.getErrorStream(), "sel:err:");
+            StreamGobbler outputGobbler = new StreamGobbler(seleniumProcess.getInputStream(), "sel:out:");
+            errorGobbler.start();
+            outputGobbler.start();
+        } catch (Exception e) {
+            // If we failed to run Selenium, skip the test
+            assumeNoException("Failed to run Selenium due to an exception", e);
+        }
+
+        long now = new Date().getTime();
+        boolean seleniumIsReady = false;
+        while (!seleniumIsReady && (new Date().getTime() - now < 5000))
+        {
+            try {
+                String urlString = "http://localhost:4444/wd/hub/status";
+                URL url = new URL(urlString);
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                connection.connect();
+                int status = connection.getResponseCode();
+                seleniumIsReady = (status == 200);
+            } catch (ConnectException e) {
+                // Server is still not ready
+            }
+        }
+        if (!seleniumIsReady) {
+            throw new Exception("Selenium did not start in time");
+        }
+    }
+
+    private static void startTestWebApp() throws Exception {
+        if (System.getProperty("jetty.port") != null) {
+            // If we're running in Maven, Jetty is already running
+            return;
+        }
+
+        // Start the Jetty server running, referencing the xssfinder-testwebapp project. Note: this is slightly
+        // different to the maven tests, which builds the war first and deploys that. This shouldn't have any
+        // impact on the tests.
+
+        Server server = new Server(8085);
+        server.setStopAtShutdown(true);
+
+        WebAppContext webAppContext = new WebAppContext();
+        webAppContext.setContextPath("/");
+        webAppContext.setResourceBase("xssfinder-testwebapp/src/main/webapp");
+        webAppContext.setDescriptor("WEB-INF/web.xml");
+        webAppContext.setClassLoader(XssFinderPhpIT.class.getClassLoader());
+        server.setHandler(webAppContext);
+
+        server.start();
+    }
+
+    private void startPhpExecutor() throws InterruptedException {
         try {
             ProcessBuilder pb = new ProcessBuilder();
             pb.command("php", "test/XssFinder/TestSite/_runserver.php");
             serverProcess = pb.start();
 
-            // any error message?
-            StreamGobbler errorGobbler = new
-                    StreamGobbler(serverProcess.getErrorStream(), "ERROR");
-
-            // any output?
-            StreamGobbler outputGobbler = new
-                    StreamGobbler(serverProcess.getInputStream(), "OUTPUT");
-
-            // kick them off
+            // Read from standard out & error
+            StreamGobbler errorGobbler = new StreamGobbler(serverProcess.getErrorStream(), "php:err:");
+            StreamGobbler outputGobbler = new StreamGobbler(serverProcess.getInputStream(), "php:out:");
             errorGobbler.start();
             outputGobbler.start();
         } catch (Exception e) {
             // If we failed to run PHP, skip the test
             assumeNoException("Failed to run PHP due to an exception", e);
         }
+
+        // Wait for the PHP executor to start up
+        //TODO: Find a better way to do this
+        Thread.sleep(1000);
     }
 
-    final class StreamGobbler extends Thread {
+    private static final class StreamGobbler extends Thread {
         InputStream is;
         String type;
 
